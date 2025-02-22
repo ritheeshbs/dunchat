7e305bc69e68e282f581b06188692882e42df860
ritheeshbs <ritheeshbs@thisux.com>
2025-02-22 12:01:26 +0530

    Added workspace & threads sections

--- landing/src/routes/+page.svelte
+++ landing/src/routes/+page.svelte
@@ -8,8 +8,26 @@
         imageURL: "/images/inbox.png",
         altText: "Inbox interface of DunChat"
     }
+
+    const workspaceSection = {
+        id: "home-workspace",
+        title: "Workspaces that bring everyone together",
+        description: "With DunChat, your workspaces keep conversations, channels, and threads organized and in the right context. \n\n*Create a space for your team, and focus on your goals*—all within a workspace that's easy to manage.",
+        imageURL: "/images/workspace.png",
+        altText: "Workspace creation interface in DunChat"
+    }
+
+    const threadsSection = {
+        id: "home-threads",
+        title: "Threads for focused conversations",
+        description: "Start threads for specific topic related discussions, *keeping everything organized without clogging up the main chat.* \n\nOnce decisions are made or a thread is no longer active, simply close it to keep the focus on what matters.",
+        imageURL: "/images/thread-conversation.png",
+        altText: "Thread conversation interface in DunChat"
+    }
 </script>
 
 <HeroSection />
 <ProblemStatementSection />
 <ExplainerSection {...introSection} />
+<ExplainerSection {...workspaceSection} />
+<ExplainerSection {...threadsSection} />
